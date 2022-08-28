6fed3caf9c73b3ab13fe42137e937a914134fc15
Muhammad Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-08-29 00:58:32 +0800

    async and await to fecth api

--- src/index.js
+++ src/index.js
@@ -34,3 +34,48 @@ const res = fetch('https://pie.dev/get')
         console.log('error bagng')
     })
 
+//async and await
+const getData = async () => {
+    const req = await fetch('https://pie.dev/get');
+    if(req.ok === false){
+        return console.log('gagal lagi sob');
+    } else {
+        const res = await req.json();
+        console.log(res);
+    }
+    // try {
+    //     const res = await req.json();
+    //     console.log(res);
+    // }catch{
+    //     console.log('gagal lagi bang');
+    // }
+
+}
+getData();
+
+//handle form
+const form = document.getElementById('form');
+const output = document.getElementById('output');
+form.addEventListener('submit', (e)=> {
+    // const getName = document.getElementById('name').value;
+    const image = document.getElementById('file').files[0];
+    e.preventDefault();
+
+    output.innerText = "uploading ...";
+    // sendToServer({getName});
+    sendToServer(image);
+});
+
+const sendToServer = async (image) => {
+    console.log(image);
+    const formData = new FormData();
+    formData.append('image', image);
+    const req = await fetch('https://pie.dev/post', {
+        method: 'POST',
+        body: formData
+    });
+    if(!req.ok) return (output.innerHTML = 'gagal kirim data');
+    const res = await req.json();
+    // output.innerText = `nama anda ${res.json.getName}`
+    output.innerHTML = `<img src="${res.files.image}">`
+}
